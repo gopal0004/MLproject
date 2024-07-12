b2df11abdcfa091ab36638f0bda50d41667dfa0e
gopal0004 <gopalbera6335@gmail.com>
2024-07-12 23:55:28 +0530

    data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from src.logger import logging
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
 
+from src.components.data_transformation import DataTransformerConfig , DataTransformation
 
 
 @dataclass
@@ -52,4 +53,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data , test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformer(train_path=train_data , test_path=test_data)
